793eb90baad39e9a82d957b8ceb4c41b90ae8ca0
Albert Ganiev <ganiev.albert.len@gmail.com>
2025-05-14 15:10:33 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Albert on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
